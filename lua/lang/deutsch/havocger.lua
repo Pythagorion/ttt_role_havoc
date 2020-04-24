015b35d40508a84a140b9ecf02911732f85bae6b
Lunex Kouhai <Lunex@online.de>
2020-04-24 16:57:40 +0200

    Name change stuff

---
+++ lua/lang/deutsch/havocger.lua
@@ -0,0 +1,12 @@
+L = LANG.GetLanguageTableReference("deutsch")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[HAVOC.name] = "Hausmeister"
+L["info_popup_" .. HAVOC.name] = [[Du bist ein Hausmeister, hilf den Verrätern indem du Beweismittel beseitigst!]]
+L["body_found_" .. HAVOC.abbr] = "Er war ein Hausmeister."
+L["search_role_" .. HAVOC.abbr] = "Diese Person war ein Hausmeister!"
+L["target_" .. HAVOC.name] = "Hausmeister"
+L["ttt2_desc_" .. HAVOC.name] = [[Der Hausmeister ist in der lage Beweismittel, welche die Verräter hinterlassen haben zu beseitigen,
+ um damit ihre Spuren zu verwischen.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
