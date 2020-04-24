273f1aafa1b182306824f1ed64bc719a38f6eae0
Lunex Kouhai <Lunex@online.de>
2020-04-24 17:36:56 +0200

    Lang file update
    Updated language files to fit the new theming better. Might go deeper on this at some point in the future.

--- lua/lang/deutsch/havoc.lua
+++ lua/lang/deutsch/havoc.lua
@@ -1,12 +1,12 @@
 L = LANG.GetLanguageTableReference("deutsch")
 
 -- GENERAL ROLE LANGUAGE STRINGS
-L[HAVOC.name] = "Hausmeister"
-L["info_popup_" .. HAVOC.name] = [[Du bist ein Hausmeister, hilf den Verrätern indem du Beweismittel beseitigst!]]
-L["body_found_" .. HAVOC.abbr] = "Er war ein Hausmeister."
-L["search_role_" .. HAVOC.abbr] = "Diese Person war ein Hausmeister!"
-L["target_" .. HAVOC.name] = "Hausmeister"
-L["ttt2_desc_" .. HAVOC.name] = [[Der Hausmeister ist in der Lage Beweismittel, welche die Verräter hinterlassen haben zu beseitigen,
+L[HAVOC.name] = "Havok"
+L["info_popup_" .. HAVOC.name] = [[Du bist Havok, Chaos in Person, hilf den Verrätern indem du Beweismittel beseitigst!]]
+L["body_found_" .. HAVOC.abbr] = "Er war ein Havok."
+L["search_role_" .. HAVOC.abbr] = "Diese Person war ein Havok!"
+L["target_" .. HAVOC.name] = "Havok"
+L["ttt2_desc_" .. HAVOC.name] = [[Havok ist in der Lage Beweismittel, welche die Verräter hinterlassen haben zu beseitigen,
  um damit ihre Spuren zu verwischen.]]
 
 -- OTHER ROLE LANGUAGE STRINGS
--- lua/lang/english/havoc.lua
+++ lua/lang/english/havoc.lua
@@ -2,12 +2,12 @@ L = LANG.GetLanguageTableReference("english")
 
 -- GENERAL ROLE LANGUAGE STRINGS
 L[HAVOC.name] = "Havoc"
-L["info_popup_" .. HAVOC.name] = [[You are a Havoc, help the traitors by cleaning a murders evidence!]]
-L["body_found_" .. HAVOC.abbr] = "They were a Havoc."
-L["search_role_" .. HAVOC.abbr] = "This person was a Havoc!"
+L["info_popup_" .. HAVOC.name] = [[You are Havoc, chaos personified, help the traitors by cleaning a murders evidence!]]
+L["body_found_" .. HAVOC.abbr] = "They were Havoc."
+L["search_role_" .. HAVOC.abbr] = "This person was Havoc!"
 L["target_" .. HAVOC.name] = "Havoc"
-L["ttt2_desc_" .. HAVOC.name] = [[The Havoc can clean evidence left behind after a murder occured, 
-he is able to erase traces left behind by the traitors.]]
+L["ttt2_desc_" .. HAVOC.name] = [[Havoc can clean evidence left behind after a murder occured, 
+they are able to erase traces left behind by the traitors.]]
 
 -- OTHER ROLE LANGUAGE STRINGS
 
