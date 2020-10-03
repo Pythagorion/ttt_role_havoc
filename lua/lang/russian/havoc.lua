83dddefd1b651c0c4e0983cc81c50642db824607
Blueberryy <igory.ygr200@gmail.com>
2020-10-03 12:28:56 +0500

    Create havoc.lua

---
+++ lua/lang/russian/havoc.lua
@@ -0,0 +1,13 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[HAVOC.name] = "Хавок"
+L["info_popup_" .. HAVOC.name] = [[Вы хавок, олицетворение хаоса, помогите предателям, убрав улики с убийствами!]]
+L["body_found_" .. HAVOC.abbr] = "Он был хавоком."
+L["search_role_" .. HAVOC.abbr] = "Этот человек был хавоком!"
+L["target_" .. HAVOC.name] = "Хавок"
+L["ttt2_desc_" .. HAVOC.name] = [[Хавок может убрать улики, оставленные после убийства, 
+он способен стереть следы, оставленные предателями.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
+
